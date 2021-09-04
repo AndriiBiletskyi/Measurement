2f4b3ad3e4231233498a4540470dc0e63a1eecc7
AndriiBiletskyi <38081603+AndriiBiletskyi@users.noreply.github.com>
2021-09-04 16:58:23 +0200

    sql3

---
+++ SQLQuery3.sql
@@ -0,0 +1,8 @@
+﻿--create index iP on dbo."1"(P);
+--CREATE NONCLUSTERED INDEX [iCzasP]
+--ON [dbo].[1] ([Czas])
+--INCLUDE ([P])
+--select Czas, P
+--from dbo."1"
+--where Czas between '01.01.2021' and '12.31.2021'
+drop index iCzasP on dbo."1"
